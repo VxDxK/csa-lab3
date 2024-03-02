a2a258b936a2ca2d329d7099a799a470a5372ef0
vadim <vadim.v.ponomarev@gmail.com>
2024-03-02 04:16:04 +0300

    Hello user

--- rebuild.sh
+++ rebuild.sh
@@ -3,3 +3,4 @@
 ./translator.py examples/sources/hello.vasm examples/bin/hello.json
 ./translator.py examples/sources/cat.vasm examples/bin/cat.json
 ./translator.py examples/sources/prob1.vasm examples/bin/prob1.json
+./translator.py examples/sources/hello_user.vasm examples/bin/hello_user.json
